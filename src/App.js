0cd7befa73d433fcc3fce65306c7c19d1cc28b78
GaetanoNapoli <gaetano.napoli@hotmail.com>
2023-12-21 14:07:33 +0100

    primo commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyForm1 from './components/MyForm1';
+import MyForm2 from './components/MyForm2';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyForm1 />
+      <MyForm2 />
+    </div>
+  );
+}
+
+export default App;
